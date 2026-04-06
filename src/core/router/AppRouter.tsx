ef4308a37bb324b0bfda7369c8581843fd92fc11
Fabiola Huayna <fabiolahuaynach@gmail.com>
2026-04-06 15:20:57 -0400

    Redireccion a Profile

--- src/core/router/AppRouter.tsx
+++ src/core/router/AppRouter.tsx
@@ -3,6 +3,7 @@ import { MainLayout } from '../layouts/MainLayout';
 import { LandingPage } from '@features/landing';
 import { LoginPage } from '@features/login';
 import { RegisterPage } from '@features/register';
+import { ProfilePage } from '@/features/profile';
 
 export const AppRouter = () => {
   return (
@@ -11,6 +12,7 @@ export const AppRouter = () => {
         <Route index element={<LandingPage />} />
         <Route path="login" element={<LoginPage />} />
         <Route path="register" element={<RegisterPage />} />
+        <Route path="profile" element={<ProfilePage />} />
       </Route>
     </Routes>
   );
