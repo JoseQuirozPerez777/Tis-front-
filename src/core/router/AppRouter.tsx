740237b2ca70aabf94a18386a2c1d09c7bc8e0d7
Mayra <choquemaqueramayra67@gmail.com>
2026-04-10 21:08:42 -0400

    ruta foto perfil

--- src/core/router/AppRouter.tsx
+++ src/core/router/AppRouter.tsx
@@ -1,8 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import { MainLayout } from '../layouts/MainLayout';
 import { LandingPage } from '@features/landing';
-import { LoginPage, ForgotPasswordPage } from '@features/login';
+import { LoginPage } from '@features/login';
 import { RegisterPage } from '@features/register';
+import { ProfilePage } from '@/features/profile';
+import { PhotoPage } from '@/features/photo/pages/PhotoPage';
+
 
 export const AppRouter = () => {
   return (
@@ -11,7 +14,8 @@ export const AppRouter = () => {
         <Route index element={<LandingPage />} />
         <Route path="login" element={<LoginPage />} />
         <Route path="register" element={<RegisterPage />} />
-        <Route path="forgot-password" element={<ForgotPasswordPage />} />
+        <Route path="profile" element={<ProfilePage />} />
+        <Route path="Photo" element={<PhotoPage />} />
       </Route>
     </Routes>
   );
