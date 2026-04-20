4d0316eb04ae0a8b11b462a724f70d882bbc4991
Mayra <choquemaqueramayra67@gmail.com>
2026-04-19 20:04:25 -0400

    página exclusiva para la funcionalidad de profesionalLink

---
+++ src/features/profesional-links/pages/ProfessionalLinksPage.tsx
@@ -0,0 +1,9 @@
+import { ProfessionalLinksForm } from '../components/ProfessionalLinksForm';
+
+export const ProfessionalLinksPage = () => {
+  return (
+    <div className="min-h-screen bg-brand-azul-profundo px-4 py-8">
+      <ProfessionalLinksForm />
+    </div>
+  );
+};
