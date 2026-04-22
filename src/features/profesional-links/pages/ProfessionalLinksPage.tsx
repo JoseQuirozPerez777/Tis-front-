a06b1b688e7bdfab85bea8e977f3154960f548c0
Mayra <choquemaqueramayra67@gmail.com>
2026-04-21 22:13:21 -0400

    correccion de pagina exclusiva para la funcionalidad

--- src/features/profesional-links/pages/ProfessionalLinksPage.tsx
+++ src/features/profesional-links/pages/ProfessionalLinksPage.tsx
@@ -3,7 +3,9 @@ import { ProfessionalLinksForm } from '../components/ProfessionalLinksForm';
 export const ProfessionalLinksPage = () => {
   return (
     <div className="min-h-screen bg-brand-azul-profundo px-4 py-8">
-      <ProfessionalLinksForm />
+      <div className="mx-auto max-w-3xl">
+        <ProfessionalLinksForm />
+      </div>
     </div>
   );
 };
