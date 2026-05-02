ad54136bc1d30baf360da8f2513580b1d35ed47c
Miguel-Huaranca-C <miktejere@gmail.com>
2026-05-01 20:52:45 -0400

    cambiar contraseña

--- src/core/router/AppRouter.tsx
+++ src/core/router/AppRouter.tsx
@@ -3,7 +3,7 @@ import { MainLayout } from '../layouts/MainLayout';
 import { DashboardLayout } from '../layouts/DashboardLayout';
 import { ProtectedRoute } from './ProtectedRoute';
 import { LandingPage } from '@features/landing';
-import { LoginPage, ForgotPasswordPage, CambiarPasswordPage } from '@features/login';
+import { LoginPage, ForgotPasswordPage, CambiarPasswordPage, ChangePasswordPage } from '@features/login';
 import { RegisterPage } from '@features/register';
 import { ProfilePage } from '@/features/profile';
 import { DashboardPage } from '@/features/dashboard';
@@ -39,6 +39,7 @@ export const AppRouter = () => {
           <Route path="/skills" element={<SkillsManagementPage />} />
           <Route path="/softskills" element={<SoftSkillPage />} />
           <Route path="/professional-links" element={<ProfessionalLinksPage />} />
+          <Route path="/change-password" element={<ChangePasswordPage />} />
         </Route>
       </Route>
     </Routes>
